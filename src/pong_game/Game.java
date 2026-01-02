c65d739cea8832cf94e3c04f86fa6bf7270d3967
ArthurAizawa <139519439+ArthurAizawa@users.noreply.github.com>
2026-01-01 21:38:38 -0300

    Implement KeyListener

--- src/pong_game/Game.java
+++ src/pong_game/Game.java
@@ -1,14 +1,17 @@
 package pong_game;
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 import java.awt.image.BufferStrategy;
 import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
-public class Game extends Canvas implements Runnable {
+public class Game extends Canvas implements Runnable, KeyListener {
 	
 	private static final long serialVersionUID = 1L;
 	public static int WIDTH = 240;
@@ -21,8 +24,8 @@ public class Game extends Canvas implements Runnable {
 	
 	public Game() {
 		this.setPreferredSize(new Dimension(WIDTH*SCALE, HEIGHT*SCALE));
-		
-		player = new Player();
+		this.addKeyListener(this);
+		player = new Player(100, HEIGHT-10);
 	}
 	
 	public static void main(String[] args) {
@@ -40,7 +43,7 @@ public class Game extends Canvas implements Runnable {
 	}
 	
 	public void tick() {
-		
+		player.tick();
 	}
 	
 	public void render() {
@@ -50,6 +53,8 @@ public class Game extends Canvas implements Runnable {
 			return;
 		}
 		Graphics g = layer.getGraphics();
+		g.setColor(Color.black);
+		g.fillRect(0, 0, WIDTH, HEIGHT);
 		player.render(g);
 		
 		
@@ -71,6 +76,34 @@ public class Game extends Canvas implements Runnable {
 			} catch(InterruptedException e) {
 				e.printStackTrace();
 			}
+		}
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = true;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = true;
+		}
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT) {
+			player.right = false;
+		}
+		else if(e.getKeyCode() == KeyEvent.VK_LEFT) {
+			player.left = false;
 		}
 		
 	}
--- src/pong_game/Player.java
+++ src/pong_game/Player.java
@@ -4,14 +4,42 @@ import java.awt.Color;
 import java.awt.Graphics;
 
 public class Player {
+	
+	public boolean right, left;
+	public int x, y;
+	public int width, height;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+		this.width = 40;
+		this.height = 10;
+	}
 	//logica do jogo
 	public void tick() {
+		//movendo o player para os lados
+		if(right) {
+			x++;
+		}
+		else if(left) {
+			x--;
+		}
+		
+		
+		//barreira da tela
+		if(x+width > Game.WIDTH) {
+			x = Game.WIDTH - width;
+		}
+		else if(x < 0) {
+			x = 0;
+		}
+		
 		
 	}
 	
 	//redenrisaçao do jogo
 	public void render(Graphics g) {
 		g.setColor(Color.blue);
-		g.fillRect(100, 120-10, 40, 10);
+		g.fillRect(x, y, width, height);
 	}
 }
